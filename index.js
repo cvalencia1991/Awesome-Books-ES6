70bccc07d7df056b8f476b8bf4d645d4e6873b81
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-26 15:27:06 -0500

    Edit index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const form = document.getElementById('booksform');
 
-
 import {books} from "./modules/Constructor.js";
-
+import {methods} from "./modules/methods.js";
 
 
 form.addEventListener('submit',(event)=>{
     const Author = document.getElementById('Author').value;
     const book =document.getElementById('Book').value;
     let bookslist = new books(Author,book);
-    alert('send it');
+    const ui = new methods();
+    ui.addbook(bookslist);
+    debugger;
     event.prevenDefault();
-})
-
+});
 
--- modules/methods.js
+++ modules/methods.js
@@ -1,7 +1,31 @@
 
+import {books} from "./Constructor.js";
 
-class methods{
-    addBook(book){
-    
+
+export class methods {
+  addbook =(event)=> {
+    const productCard = document.getElementById('booklist');
+    const element = document.createElement('div');
+    element.classList.add('bookstyle');
+    element.innerHTML = `
+      "${books.Book}" by ${books.Author}
+      <div>
+      <button type="button" name="deletebook">remove</button>
+      <div>
+          `;
+    productCard.appendChild(element);
+    event.preventDefault();
     }
-}
+/*
+    resetform() {
+      document.getElementById('books').reset();
+    }
+
+    removebook(element) {
+      if (element.name === 'deletebook') {
+        element.parentElement.parentElement.remove();
+      }
+    } */
+
+  }
+
