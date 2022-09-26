47ee15eb3d1a5ab7cee47e8838953143b5bd618b
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-26 16:56:44 -0500

    Edit Delete Book list item

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 
 
 const form = document.getElementById('booksform');
+const cardbook = document.getElementById('booklist');
 
 import {books} from "./modules/Constructor.js";
 import {methods} from "./modules/methods.js";
 
+const ui = new methods();
 
 form.addEventListener('submit',(event)=>{
     const Author = document.getElementById('Author').value;
     const book =document.getElementById('Book').value;
     let bookslist = new books(Author,book);
-    const ui = new methods();
     ui.addbook(bookslist);
     event.preventDefault();
 });
+
+cardbook.addEventListener('click',(e)=>{
+    ui.removebook(e.target);
+});
+
 
--- modules/methods.js
+++ modules/methods.js
@@ -1,7 +1,4 @@
 
-/* import {books} from "./Constructor.js"; */
-
-
 export class methods {
   addbook =(books)=> {
     const productCard = document.getElementById('booklist');
@@ -14,18 +11,15 @@ export class methods {
       <div>
           `;
     productCard.appendChild(element);
-/*     debugger; */
     }
-/*
     resetform() {
       document.getElementById('books').reset();
     }
-
     removebook(element) {
       if (element.name === 'deletebook') {
         element.parentElement.parentElement.remove();
       }
-    } */
+    }
 
   }
 
