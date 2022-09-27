92aa2bc95aae7872266939004c32b1d0ef93bfcf
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-26 19:36:51 -0500

    Edit addNew link

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-
 const form = document.getElementById('booksform');
 const cardbook = document.getElementById('booklist');
+const addnew = document.getElementById('addnew');
+const list = document.getElementById('list');
+const contact = document.getElementById('contact');
+const styleHeader = document.getElementById('headtittle');
+const statusbooks = document.getElementById('booksform');
 
 
 import {books} from "./modules/Constructor.js";
 import {methods} from "./modules/methods.js";
 import { DateTime } from "./node_modules/luxon/build/es6/luxon.js";
+
 
 const ui = new methods();
 document.getElementById('date').innerText = DateTime.now();
@@ -24,5 +28,25 @@ cardbook.addEventListener('click',(e)=>{
     ui.removebook(e.target);
 });
 
+list.addEventListener('click',(event)=>{
+    styleHeader.textContent = 'All Awesome Books';
+    statusbooks.style.display='none';
+    cardbook.style.display='flex';
+    event.preventDefault();
+});
+
+addnew.addEventListener('click',(event)=>{
+    styleHeader.textContent = 'Add New Book';
+    statusbooks.style.display='flex';
+    cardbook.style.display='none';
+    event.preventDefault();
+});
+
+contact.addEventListener('click',(event)=>{
+    styleHeader.textContent = 'Contact information';
+    statusbooks.style.display='none';
+    cardbook.style.display='none';
+    event.preventDefault();
+});
 
 
--- modules/methods.js
+++ modules/methods.js
@@ -1,4 +1,3 @@
-
 export class methods {
   addbook =(books)=> {
     const productCard = document.getElementById('booklist');
